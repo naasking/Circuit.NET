390293851c4c83c91bddfed026bdc2388233c23f
Sandro Magi <none@none>
2020-05-20 14:37:23 -0400

    thread a run queue that's used to delay computations

--- Circuit/Circuit.cs
+++ Circuit/Circuit.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 
 namespace Circuit
 {
@@ -11,9 +12,9 @@ namespace Circuit
     public struct Circuit<T0, T1>
     {
         // a circuit is delegate network in CPS form
-        internal Action<T0, Action<T1>> k;
+        internal Action<T0, Queue<Action>, Action<T1>> k;
 
-        internal Circuit(Action<T0, Action<T1>> k) =>
+        internal Circuit(Action<T0, Queue<Action>, Action<T1>> k) =>
             this.k = k;
 
         /// <summary>
@@ -25,7 +26,7 @@ namespace Circuit
         public Circuit<T0, T2> Connect<T2>(Circuit<T1, T2> next)
         {
             var k = this.k;
-            return new Circuit<T0, T2>((x0, k2) => k(x0, x1 => next.k(x1, x2 => k2(x2))));
+            return new Circuit<T0, T2>((x0, q, k2) => k(x0, q, x1 => next.k(x1, q, x2 => k2(x2))));
         }
 
         /// <summary>
@@ -37,7 +38,23 @@ namespace Circuit
         public Circuit<T0, T2> Select<T2>(Func<T1, T2> next)
         {
             var k = this.k;
-            return new Circuit<T0, T2>((x0, k2) => k(x0, x1 => k2(next(x1))));
+            return new Circuit<T0, T2>((x0, q, k2) => k(x0, q, x1 => k2(next(x1))));
+        }
+
+        /// <summary>
+        /// Transform the circuit output.
+        /// </summary>
+        /// <typeparam name="T2"></typeparam>
+        /// <param name="next"></param>
+        /// <returns></returns>
+        public Circuit<T0, T1> Where(Func<T1, bool> predicate)
+        {
+            var k = this.k;
+            return new Circuit<T0, T1>((x0, q, k2) => k(x0, q, x1 =>
+            {
+                if (predicate(x1))
+                    k2(x1);
+            }));
         }
 
         /// <summary>
@@ -49,7 +66,7 @@ namespace Circuit
         public Circuit<T0, (T1, T2)> Merge<T2>(Circuit<T0, T2> next)
         {
             var k = this.k;
-            return new Circuit<T0, (T1, T2)>((x0, k12) => next.k(x0, x2 => k(x0, x1 => k12((x1, x2)))));
+            return new Circuit<T0, (T1, T2)>((x0, q, k12) => next.k(x0, q, x2 => k(x0, q, x1 => k12((x1, x2)))));
         }
 
         /// <summary>
@@ -63,7 +80,7 @@ namespace Circuit
             //FIXME: if 'this' depends upon 'next' somehow, then next.circuit is called twice for every update.
             //This only happens if user supplies a delegate that explicitly invokes Circuit.Run.
             var k = this.k;
-            return new Circuit<T0, T3>((x0, k12) => next.k(x0, x2 => k(x0, x1 => k12(select(x1, x2)))));
+            return new Circuit<T0, T3>((x0, q, k12) => next.k(x0, q, x2 => k(x0, q, x1 => k12(select(x1, x2)))));
         }
 
         /// <summary>
@@ -72,16 +89,8 @@ namespace Circuit
         /// <returns>A signal with a delayed output.</returns>
         public Circuit<T0, T1> Delay()
         {
-            Action<T0, Action<T1>> next = k;
-            T0 xo0 = default(T0);
-            Action<T1> ko1 = null;
-            return new Circuit<T0, T1>((x0, k1) =>
-            {
-                if (ko1 != null)
-                    next.Invoke(xo0, ko1);
-                xo0 = x0;
-                ko1 = k1;
-            });
+            var k = this.k;
+            return new Circuit<T0, T1>((x0, q, k1) => q.Enqueue(() => k(x0, q, k1)));
         }
 
         /// <summary>
@@ -93,7 +102,7 @@ namespace Circuit
         public Circuit<(T0, T2), (T1, T2)> First<T2>()
         {
             var k = this.k;
-            return new Circuit<(T0, T2), (T1, T2)>((x02, k12) => k(x02.Item1, x1 => k12((x1, x02.Item2))));
+            return new Circuit<(T0, T2), (T1, T2)>((x02, q, k12) => k(x02.Item1, q, x1 => k12((x1, x02.Item2))));
         }
 
         /// <summary>
@@ -102,7 +111,10 @@ namespace Circuit
         /// <returns></returns>
         public void Run(T0 input, Action<T1> output)
         {
-            this.k?.Invoke(input, output);
+            var q = new Queue<Action>(2);
+            this.k?.Invoke(input, q, output);
+            while (q.Count > 0)
+                q.Dequeue().Invoke();
         }
 
         /// <summary>
@@ -120,8 +132,8 @@ namespace Circuit
         /// </remarks>
         public void Register(Action<T1> output, Action<EventHandler<T0>> register)
         {
-            var k = this.k;
-            register((o, e) => k(e, output));
+            var circ = this;
+            register((o, e) => circ.Run(e, output));
         }
 
         /// <summary>
@@ -160,7 +172,7 @@ namespace Circuit
         /// <param name="init"></param>
         /// <returns></returns>
         public static Circuit<T0, T1> Create<T0, T1>(Func<T0, T1> init) =>
-            new Circuit<T0, T1>((x0, k1) => k1(init(x0)));
+            new Circuit<T0, T1>((x0, q, k1) => k1(init(x0)));
 
         //public static Reactive<T, Time> Time<T>();
         //public static Reactive<float, float> IntegralFloat();
@@ -173,7 +185,7 @@ namespace Circuit
         /// <param name="value"></param>
         /// <returns></returns>
         public static Circuit<Empty, T> Constant<T>(T value) =>
-            new Circuit<Empty, T>((_, k) => k(value));
+            new Circuit<Empty, T>((_, q, k) => k(value));
 
         /// <summary>
         /// Circuit outputting a constant value.
@@ -182,7 +194,7 @@ namespace Circuit
         /// <param name="value"></param>
         /// <returns></returns>
         public static Circuit<Empty, T> Sampler<T>(Func<T> sampler) =>
-            new Circuit<Empty, T>((_, k) => k(sampler()));
+            new Circuit<Empty, T>((_, q, k) => k(sampler()));
 
         /// <summary>
         /// Run a circuit.
@@ -199,7 +211,7 @@ namespace Circuit
         /// <param name="filter"></param>
         /// <returns></returns>
         public static Circuit<T0, T0> Where<T0>(this Circuit<T0, T0> circuit, Func<T0, bool> filter) =>
-            new Circuit<T0, T0>((x0, k0) =>
+            new Circuit<T0, T0>((x0, q, k0) =>
             {
                 if (filter(x0))
                     k0(x0);
@@ -227,7 +239,7 @@ namespace Circuit
         /// <returns></returns>
         public static Circuit<T0, Event<T1>> Merge<T0, T1>(this Circuit<T0, Event<T1>> first, Circuit<T0, Event<T1>> second)
         {
-            return new Circuit<T0, Event<T1>>((x0, k1) => first.k(x0, x1 => second.k(x0, x2 => k1(x1.HasValue ? x1 : x2))));
+            return new Circuit<T0, Event<T1>>((x0, q, k1) => first.k(x0, q, x1 => second.k(x0, q, x2 => k1(x1.HasValue ? x1 : x2))));
         }
 
         /// <summary>
@@ -241,14 +253,14 @@ namespace Circuit
         /// <returns></returns>
         public static Circuit<T0, T1> Switch<T0, T1, T2>(this Circuit<T0, (T1, Event<T2>)> source, Func<T2, Circuit<T0, T1>> map)
         {
-            Action<T0, Action<T1>> k = null;
-            return new Circuit<T0, T1>((x0, k1) => source.k(x0, x1e2 =>
+            Action<T0, Queue<Action>, Action<T1>> k = null;
+            return new Circuit<T0, T1>((x0, q, k1) => source.k(x0, q, x1e2 =>
             {
                 // switch to another signal when an event occurs
                 if (x1e2.Item2.HasValue)
                     k = map(x1e2.Item2.Value).k;
                 if (k != null)
-                    k(x0, k1);
+                    k(x0, q, k1);
                 else
                     k1(x1e2.Item1);
             }));
@@ -265,7 +277,7 @@ namespace Circuit
         /// <returns></returns>
         public static Circuit<T0, T1> Loop<T0, T1, T2>(this Circuit<(T0, T2), (T1, T2)> source, T2 x2 = default(T2))
         {
-            return new Circuit<T0, T1>((x0, k1) => source.k((x0, x2), x12 =>
+            return new Circuit<T0, T1>((x0, q, k1) => source.k((x0, x2), q, x12 =>
             {
                 x2 = x12.Item2;
                 k1(x12.Item1);
